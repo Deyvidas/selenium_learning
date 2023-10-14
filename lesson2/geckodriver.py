e3a51dadcfa38ee65212100233eae4fe82df6b59
Deyvidas <devidbanyavichyus@yandex.ru>
2023-10-15 01:05:29 +0400

    Lesson 8, how to: send value, click (cursor show).
    In this lesson I learned how to:
        - clear input field;
        - insert value into field;
        - check value attribute of element (check if passed value in field);
        - use cursor with ActionChains(), in particular how to:
            - place cursor into middle of element in `move_to_element()`;
            - move cursor with `move_by_offset()`;
            - how to click with ActionChains();
            - IMPORTANT ActionChains required execute method .perform() without him
            passed function is added into waiting list and not executed.

--- lesson2/geckodriver.py
+++ lesson2/geckodriver.py
@@ -6,6 +6,24 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 from config.settings import firefox_profile_folder
 
+
+# Что-бы видеть курсор (https://stackoverflow.com/a/76068212/20084479)
+cursor_script = '''
+var cursor = document.createElement('div');
+cursor.style.position = 'absolute';
+cursor.style.zIndex = '9999';
+cursor.style.width = '5px';
+cursor.style.height = '5px';
+cursor.style.borderRadius = '50%';
+cursor.style.backgroundColor = 'red';
+cursor.style.pointerEvents = 'none';
+document.body.appendChild(cursor);
+
+document.addEventListener('mousemove', function(e) {
+  cursor.style.left = e.pageX - 5 + 'px';
+  cursor.style.top = e.pageY - 5 + 'px';
+});
+'''
 options = Options()
 options.add_argument('--profile')
 options.add_argument(firefox_profile_folder())
---
+++ lesson8/send_and_click_practice.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.common.action_chains import ActionChains
+
+from lesson2.geckodriver import driver, cursor_script
+
+
+driver.get('https://practice.expandtesting.com/form-validation')
+driver.execute_script(cursor_script)  # Для отображения движения курсора.
+ac = ActionChains(driver)
+
+
+# Вбиваем имя.
+name = driver.find_element('xpath', '//input[@name="ContactName"]')
+name.clear()
+ac.send_keys_to_element(name, 'Pythonist').perform()
+assert name.get_attribute('value') == 'Pythonist'
+
+# Вбиваем номер телефона.
+number = driver.find_element('xpath', '//input[@name="contactnumber"]')
+number.clear()
+ac.send_keys_to_element(number, '012-3456789').perform()
+assert number.get_attribute('value') == '012-3456789'
+
+# Выбираем удобную дату доставки.
+pickup_date = driver.find_element('xpath', '//input[@name="pickupdate"]')
+pickup_date.clear()
+ac.send_keys_to_element(pickup_date, '12282024').perform()
+assert pickup_date.get_attribute('value') == '2024-12-28'
+
+# Выбираем удобный способ оплаты.
+payment = driver.find_element('xpath', '//select[@name="payment"]')
+cash = driver.find_element('xpath', '//option[@value="cashondelivery"]')
+card = driver.find_element('xpath', '//option[@value="card"]')
+cash.click()
+assert payment.get_attribute('value') == 'cashondelivery'
+
+# Наводимся на кнопку и жмякаем.
+register_button = driver.find_element('xpath', '//button[text()=" Register "]')
+(
+    ac.move_to_element(register_button)
+    .move_by_offset(0, 99)
+    .move_by_offset(-99, -99)
+    .move_by_offset(99, -99)
+    .move_by_offset(99, 99)
+    .move_by_offset(-99, 99)
+    .move_by_offset(0, -99)
+    .click().perform()
+)
