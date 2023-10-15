9715d8b79b8b11d1bec16f665f1de89c9045f601
Deyvidas <devidbanyavichyus@yandex.ru>
2023-10-15 21:27:35 +0400

    Lesson 9, settings of firefox and google browser.
    In this lesson I learned how to:
        - run browser without GUI (graphical interface);
        - run browser in incognito mode;
        - run browser with opened devtools (only for firefox);
        - define width and height of window before and after browser opening;
        - disable caching of data in memory;
        - change default directory when browser save downloaded files;
    
    I trough important, that for every browser which we open, we need to specifies
    size of window, because if don`t do that, maybe some problem in future
    For example:
        In our code there is function which move cursor to te element and use
        pixels for that, and if we use 1920 * 1080 window, all work correct
        but if make window size 500 * 500 this function does`t work! Because
        element which function try to click is smaller and click did not get
        to the element.

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-def firefox_profile_folder() -> str:
-    """Create and return path where firefox profile will saved if not exist."""
-    path = str(BASE_DIR / 'firefox_profile')
+def set_new_folder_or_get_existent(suffix: str) -> str:
+    """Create if not exist and return path BASE_DIR/<suffix>."""
+    path = str(BASE_DIR / suffix)
     if not os.path.isdir(path):
         os.mkdir(path)
     return path
--- lesson2/geckodriver.py
+++ lesson2/geckodriver.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.service import Service
 
 from webdriver_manager.firefox import GeckoDriverManager
 
-from config.settings import firefox_profile_folder
+from config.settings import set_new_folder_or_get_existent
 
 
 # Что-бы видеть курсор (https://stackoverflow.com/a/76068212/20084479)
@@ -24,8 +24,27 @@ document.addEventListener('mousemove', function(e) {
   cursor.style.top = e.pageY - 5 + 'px';
 });
 '''
+
+# Настройка браузера перед запуском.
 options = Options()
+
+# Указываем папку куда firefox будет скачивать файлы.
+download_dir = set_new_folder_or_get_existent('firefox_download')
+options.set_preference('browser.download.dir', download_dir)
+options.set_preference('browser.download.folderList', 2)
+
+# --devtools -> Open DevTools on initial load (запуск с панелью разработки).
+options.add_argument('--devtools')
+
+# --width=<int> --height=<int> указать какого размера должно быть окно
+# браузера при его запуске (данный способ предпочтительнее).
+options.add_argument('--width=1920')
+options.add_argument('--height=1080')
+
+# Загрузка профиля firefox или создание нового. Профиль нужен для хранения
+# настроек, закладок, паролей, ... (профиль необходим даже в режиме private!)
 options.add_argument('--profile')
-options.add_argument(firefox_profile_folder())
+options.add_argument(set_new_folder_or_get_existent('firefox_profile'))
+
 service = Service(executable_path=GeckoDriverManager().install())
 driver = webdriver.Firefox(options=options, service=service)
